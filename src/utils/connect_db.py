8717045b61a2494e43bbc86bc2ca56510a37f2da
Dthenewbie <chch8610@gmail.com>
2025-02-20 03:23:35 +0800

    change the gcp-db

--- src/utils/connect_db.py
+++ src/utils/connect_db.py
@@ -1,20 +1,20 @@
 import pymysql
 def connect_db():
     try:
-        conn = pymysql.connect(
-            host = 'host.docker.internal',
-            port = 3306,
-            user = 'root',
-            passwd = 'password',
-            db = 'Anti_Fraud'
-        )
         # conn = pymysql.connect(
-        # host = '35.229.167.236',
-        # port = 3306,
-        # user = 'root',
-        # passwd = 'my-secret-pw',
-        # db = 'Anti_Fraud'
-        #   )
+        #     host = 'host.docker.internal',
+        #     port = 3306,
+        #     user = 'root',
+        #     passwd = 'password',
+        #     db = 'Anti_Fraud'
+        # )
+        conn = pymysql.connect(
+        host = '35.229.167.236',
+        port = 3306,
+        user = 'root',
+        passwd = 'my-secret-pw',
+        db = 'Anti_Fraud'
+          )
         return conn
     except Exception as e:
         print(f"Error connecting to database: {e}")
