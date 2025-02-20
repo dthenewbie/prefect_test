927f9c2161181e1c1d7bf657a4807a5904059bd8
Dthenewbie <chch8610@gmail.com>
2025-02-20 18:18:37 +0800

    use VM internal IP

--- src/utils/connect_db.py
+++ src/utils/connect_db.py
@@ -2,7 +2,7 @@ import pymysql
 def connect_db():
     try:
         conn = pymysql.connect(
-        host = '104.199.228.45',
+        host = '10.140.0.2',
         port = 3306,
         user = 'root',
         passwd = 'my-secret-pw',
