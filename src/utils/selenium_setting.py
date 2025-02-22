6ceb0bebb6d17009f89cb12588da952a80fbb790
Dthenewbie <chch8610@gmail.com>
2025-02-22 20:18:26 +0800

    change db IP

--- src/utils/selenium_setting.py
+++ src/utils/selenium_setting.py
@@ -24,7 +24,7 @@ def setup_driver():
     chrome_options.add_argument("--enable-unsafe-swiftshader")
     chrome_options.add_argument('--user-agent=%s' % userAgent)
     driver = webdriver.Remote(
-        command_executor=f"http://34.57.171.89:14444/wd/hub",
+        command_executor=f"http://104.199.228.45:14444/wd/hub",
         options=chrome_options,
     )
     return driver
